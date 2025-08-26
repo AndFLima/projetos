1b9098e6e29888801a2299e5fb68b3924099a0bb
AndFLima <lima.afl@live.com>
2025-08-25 23:58:45 -0300

    Visando uma melhor esperiência para o usuário, foi introdusido o JavaScript no projeto

---
+++ scripts.js
@@ -0,0 +1,47 @@
+const prevButton = document.getElementById('prev')
+const nextButton = document.getElementById('next')
+const itens = document.querySelectorAll('.item')
+const ponto = document.querySelectorAll('.ponto')
+const numerosIndicadores = document.getElementById('numeros')
+const lista = document.getElementById('lista-carrocel')
+
+let ativo = 0;
+const total = itens.length
+let timer;
+
+function atualizar(direção) {
+    document.querySelector('.item.ativo').classList.remove('ativo')
+    document.querySelector('.ponto.ativo').classList.remove('ativo')
+
+    if(direção > 0) {
+        ativo = ativo + 1
+
+        if(ativo === total) {
+            ativo = 0
+        }
+    }
+    else if(direção < 0) {
+        ativo = ativo -1
+
+        if(ativo < 0) {
+            ativo = total -1
+        }
+    }
+    
+    itens[ativo].classList.add('ativo')
+    ponto[ativo].classList.add('ativo')
+
+    numerosIndicadores.innerHTML = String(ativo + 1).padStart(2,'0')
+}
+
+prevButton.addEventListener('click', function() {
+    atualizar(-1)
+})
+nextButton.addEventListener('click', function() {
+    atualizar(1)
+})
+
+
+setInterval(() => {
+    atualizar(1)
+}, 5000);
